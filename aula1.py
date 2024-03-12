8c2dc8ebed6779c490a3ab90d11f5d8444d40ee7
David Medeiros <davidmedeiros94@hotmail.com>
2024-03-12 20:52:04 -0300

    Class26

--- aula1.py
+++ aula1.py
@@ -2,8 +2,17 @@ print("Hello, World")
 # Comentário
 """Não é comentário"""
 
-print(type(1))
-print(type(0.1))
-print(type("David"))
-print(type(10==10))
+# print(type(1))
+# print(type(0.1))
+# print(type("David"))
+# print(type(10==10))
+
+nome_completo = "David Medeiros"
+print(nome_completo)
+idade = 30
+verificacao = idade >= 18
+
+print("Nome:", nome_completo,
+      "Idade:", idade,
+      "É maior de idade? :", verificacao)
 
